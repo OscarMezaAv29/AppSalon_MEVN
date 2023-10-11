ab48b16ae479cb25069dac0efe27eb96f3220cf0
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-11 16:30:14 -0600

    Validar ObjectID's de MongoDB

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 import { services } from '../data/beautyServices.js'
 import Services from '../models/Services.js'
+import { validateObjectId } from '../utils/index.js'
 
 const createService = async (req, res) => {
     if(Object.values(req.body).includes('')) {
@@ -30,13 +31,7 @@ const getServices = (req, res) => {
 const getServiceById = async (req, res) => {
     const { id } = req.params
     // Validar un object id
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        const error = new Error('El ID no es válido')
-
-        return res.status(400).json({
-            msg: error.message
-        })
-    }
+    if(validateObjectId(id, res)) return
 
     // Validar que exista
     const service = await Services.findById(id)
@@ -55,13 +50,7 @@ const getServiceById = async (req, res) => {
 const updateService = async (req, res) => {
     const { id } = req.params
     // Validar un object id
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        const error = new Error('El ID no es válido')
-
-        return res.status(400).json({
-            msg: error.message
-        })
-    }
+    if(validateObjectId(id, res)) return
 
     // Validar que exista
     const service = await Services.findById(id)
