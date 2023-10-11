e132398007555fb52f183f170a1b4852f47446b0
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-11 15:56:47 -0600

    Obtener un Servicio por su ID

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -1,9 +1,11 @@
+import mongoose from 'mongoose'
 import { services } from '../data/beautyServices.js'
 import Services from '../models/Services.js'
 
 const createService = async (req, res) => {
     if(Object.values(req.body).includes('')) {
         const error = new Error('Todos los campos son obligatorios')
+
         return res.status(400).json({
             msg: error.message
         })
@@ -25,7 +27,33 @@ const getServices = (req, res) => {
     res.json(services)
 }
 
+const getServiceById = async (req, res) => {
+    const { id } = req.params
+    // Validar un object id
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        const error = new Error('El ID no es válido')
+
+        return res.status(400).json({
+            msg: error.message
+        })
+    }
+
+    // Validar que exista
+    const service = await Services.findById(id)
+    if(!service) {
+        const error = new Error('El Servicio no existe')
+
+        return res.status(404).json({
+            msg: error.message
+        })
+    }
+
+    // Mostrar el servicio
+    res.json(service)
+}
+
 export {
     createService,
-    getServices
+    getServices,
+    getServiceById
 }
