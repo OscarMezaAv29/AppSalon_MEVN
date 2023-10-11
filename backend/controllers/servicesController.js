98e663ee847f42cca73b51a3f6a93528b5c8c3ab
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-11 15:32:33 -0600

    Insertando el Primer Servicio en la Base de datos

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -1,4 +1,5 @@
 import { services } from '../data/beautyServices.js'
+import Services from '../models/Services.js'
 
 const createService = async (req, res) => {
     if(Object.values(req.body).includes('')) {
@@ -6,6 +7,17 @@ const createService = async (req, res) => {
         return res.status(400).json({
             msg: error.message
         })
+    }
+
+    try {
+        const service = new Services(req.body)
+        await service.save()
+
+        res.json({
+            msg: 'El servicio se creó correctamente'
+        })
+    } catch (error) {
+        console.log(error);
     }
 }
 
