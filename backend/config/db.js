8806b3c8a8ed2e81e94358acec9f16c5a578c487
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-11 13:12:03 -0600

    Agregado git ignore

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose'
 export const db = async () => {
     try {
         const db = await mongoose.connect(process.env.MONGO_URI)
-        console.log('Se conectó correctamente');
+        console.log(db.connection);
+        const url = `${db.connection.host}:${db.connection.port}`
+        console.log('MongoDB se conectó correctamente: ', url);
     } catch (error) {
         console.log(`Error: ${error.message}`);
         process.exit(1)
