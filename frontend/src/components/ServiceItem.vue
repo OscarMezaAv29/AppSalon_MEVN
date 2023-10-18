3cd2eeb2a261e3f4e38673ea035816ddcf164ebf
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-18 10:05:43 -0600

    Evitar Registros Duplicados

--- frontend/src/components/ServiceItem.vue
+++ frontend/src/components/ServiceItem.vue
@@ -13,11 +13,15 @@
 
 <template>
   <div
-    class="p-5 space-y-5 rounded-lg cursor-pointer bg-white"
+    class="p-5 space-y-5 rounded-lg cursor-pointer"
+    :class="appointments.isServiceSelected(service._id) ? 'bg-blue-500 text-white' : 'bg-white'"
     @click="appointments.onServiceSelected(service)"
   >
     <p class="text-2xl font-light">{{ service.name }}</p>
 
-    <p class="text-4xl font-black text-blue-600">{{ formatCurrency(service.price) }}</p>
+    <p
+      class="text-4xl font-black "
+      :class="appointments.isServiceSelected(service._id) ? 'bg-blue-500 text-white' : 'text-blue-600'"
+    >{{ formatCurrency(service.price) }}</p>
   </div>
 </template>
