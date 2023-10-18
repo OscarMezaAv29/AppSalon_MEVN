c6b423a8c96fe59f0fcd5f905d53c8e303ab320f
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-18 15:23:56 -0600

    Mostrando las Horas de Reservación

--- frontend/src/stores/appointments.js
+++ frontend/src/stores/appointments.js
@@ -1,10 +1,19 @@
 import { defineStore } from "pinia";
-import { ref,computed } from "vue";
+import { ref,computed, onMounted } from "vue";
 
 export const useAppointmentsStore = defineStore('appointments', () => {
 
     const services= ref([])
     const date = ref('')
+    const hours = ref([])
+
+    onMounted(() => {
+        const startHour = 10
+        const endHour = 19
+        for(let hour = startHour; hour <= endHour; hour++) {
+            hours.value.push(hour + ':00')
+        }
+    })
 
 
     function onServiceSelected(service) {
@@ -32,6 +41,7 @@ export const useAppointmentsStore = defineStore('appointments', () => {
     return {
         services,
         date,
+        hours,
         onServiceSelected,
         isServiceSelected,
         noServicesSelected,
