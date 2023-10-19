881eb3b6b79b9fa37b43b0256e13bceb69d9d524
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-19 11:53:21 -0600

    Creando el Router de Autenticación y Registro de Usuarios

---
+++ backend/controllers/authController.js
@@ -0,0 +1,10 @@
+const register = async (req, res) => {
+    console.log('Desde register');
+}
+
+
+
+
+export {
+    register
+}
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { register } from '../controllers/authController.js'
+
+const router = express.Router()
+
+// Rutas de autenticación y registros de usuarios
+
+router.post('/register', register)
+
+
+
+
+export default router
