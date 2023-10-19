f0390cd67eaf372ff0b5e0d6bd0aac8c5163537e
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-19 13:32:11 -0600

    Enviar email de prueba

--- backend/emails/authEmailService.js
+++ backend/emails/authEmailService.js
@@ -1,5 +1,21 @@
 import { createTransport } from '../config/nodemailer.js'
 
 export async function sendEmailVerification() {
-    console.log('Desde sendEmailVerification');
+    const transporter = createTransport(
+        "sandbox.smtp.mailtrap.io",
+        2525,
+        "13fecf5ee9c6bd",
+        "a292f1da6ab0a8"
+    )
+
+    // Enviar el email
+    const info = await transporter.sendMail({
+        from: 'AppSalon',
+        to: 'correo@correo.com',
+        subject: "AppSalon - Confirma tu cuenta",
+        text: "AppSalon - Confirma tu cuenta",
+        html: "Probando email"
+    })
+
+    console.log('Mensaje enviado', info.messageId);
 }
