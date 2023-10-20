dfb90c0b3b96a430531d10d05214814e54c0033c
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-20 15:54:49 -0600

    Mostrando los Enlaces hacia la página de Registro y Autenticación

--- frontend/src/views/auth/AuthLayout.vue
+++ frontend/src/views/auth/AuthLayout.vue
@@ -1,12 +1,24 @@
 <script setup>
 
+    const authRoutes = [
+        { name: 'register', text: 'Crear una cuenta' },
+        { name: 'login', text: 'Iniciar Sesión' }
+    ]
 
 </script>
 
 <template>
-  <div>
+  <div class="mx-auto lg:w-4/5 mt-20">
     <h1>Desde Auth Layout</h1>
 
     <RouterView />
+
+    <nav class="mt-10 flex flex-col items-center space-y-5 lg:flex-row lg:justify-between lg:space-y-0">
+        <RouterLink 
+            v-for="authRoute in authRoutes"
+            class="uppercase font-bold text-white"
+            :to="{name: authRoute.name}"
+        >{{ authRoute.text }}</RouterLink>
+    </nav>
   </div>
 </template>
