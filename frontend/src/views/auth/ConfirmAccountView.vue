74aa10a14a57d4142f0448379bb05ddc78fdd721
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-20 15:32:40 -0600

    Definiendo Rutas y Layout de Registro de Usuarios y Autenticación

---
+++ frontend/src/views/auth/ConfirmAccountView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Desde ConfirmAccountView</h1>
+
+  </div>
+</template>
---
+++ frontend/src/views/auth/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Desde LoginView</h1>
+
+  </div>
+</template>
