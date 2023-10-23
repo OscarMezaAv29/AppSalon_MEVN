3b75861e479309c0bde57385e2d11a142efa4d34
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-23 09:50:39 -0600

    Mostrando la respuesta del backend en el Toast

--- frontend/src/views/auth/RegisterView.vue
+++ frontend/src/views/auth/RegisterView.vue
@@ -1,11 +1,18 @@
 <script setup>
+import { inject } from 'vue'
 import AuthAPI from '../../api/AuthAPI'
 
+const toast = inject('toast')
 
 
-const handleSubmit = async ({password_confirm, ...data}) => {
+const handleSubmit = async ({ password_confirm, ...formData }) => {
   try {
-    await AuthAPI.register(data)
+    const { data } = await AuthAPI.register(formData)
+    toast.open({
+      message: data.msg,
+      type: 'success'
+    })
+
   } catch (error) {
     console.log(error);
   }
@@ -14,65 +21,37 @@ const handleSubmit = async ({password_confirm, ...data}) => {
 </script>
 
 <template>
-    <h1 class="text-6xl font-extrabold text-white text-center mt-10">Crea una cuenta</h1>
-    <p class="text-2xl text-white text-center my-5">Crea una cuenta en AppSalón</p>
+  <h1 class="text-6xl font-extrabold text-white text-center mt-10">Crea una cuenta</h1>
+  <p class="text-2xl text-white text-center my-5">Crea una cuenta en AppSalón</p>
 
 
-    <FormKit
-      type="form"
-      :actions="false"
-      incomplete-message="No se pudo enviar, revisa las notificaciones"
-      @submit="handleSubmit"
-    >
-      <FormKit 
-        type="text"
-        label="Nombre"
-        name="name"
-        placeholder="Tu nombre"
-        validation="required|length:3"
-        :validation-messages="{
-          required: 'El nombre es obligatorio',
-          length: 'El nombre es muy corto'
-        }"
-      />
+  <FormKit type="form" :actions="false" incomplete-message="No se pudo enviar, revisa las notificaciones"
+    @submit="handleSubmit">
+    <FormKit type="text" label="Nombre" name="name" placeholder="Tu nombre" validation="required|length:3"
+      :validation-messages="{
+        required: 'El nombre es obligatorio',
+        length: 'El nombre es muy corto'
+      }" />
 
-      <FormKit 
-        type="email"
-        label="Email"
-        name="email"
-        placeholder="Email de Registro"
-        validation="required|email"
-        :validation-messages="{
-          required: 'El nombre es obligatorio',
-          email: 'Email no válido'
-        }"
-      />
+    <FormKit type="email" label="Email" name="email" placeholder="Email de Registro" validation="required|email"
+      :validation-messages="{
+        required: 'El nombre es obligatorio',
+        email: 'Email no válido'
+      }" />
 
-      <FormKit 
-        type="password"
-        label="Password"
-        name="password"
-        placeholder="Password de Usuario - Min 8 Caracteres"
-        validation="required|length:8"
-        :validation-messages="{
-          required: 'El password es obligatorio',
-          length: 'El password debe contener al menos 8 caracteres'
-        }"
-      />
+    <FormKit type="password" label="Password" name="password" placeholder="Password de Usuario - Min 8 Caracteres"
+      validation="required|length:8" :validation-messages="{
+        required: 'El password es obligatorio',
+        length: 'El password debe contener al menos 8 caracteres'
+      }" />
 
-      <FormKit 
-        type="password"
-        label="Repetir Password"
-        name="password_confirm"
-        placeholder="Repite el password"
-        validation="required|confirm"
-        :validation-messages="{
-          required: 'El password es obligatorio',
-          confirm: 'Los passwords no son iguales'
-        }"
-      />
+    <FormKit type="password" label="Repetir Password" name="password_confirm" placeholder="Repite el password"
+      validation="required|confirm" :validation-messages="{
+        required: 'El password es obligatorio',
+        confirm: 'Los passwords no son iguales'
+      }" />
 
-      <FormKit type="submit">Crear Cuenta</FormKit>
+    <FormKit type="submit">Crear Cuenta</FormKit>
 
-    </FormKit>
+  </FormKit>
 </template>
