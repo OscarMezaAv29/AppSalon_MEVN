fd6a091213b066a905fa70993834ee225f0396cb
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-23 10:23:32 -0600

    Mostrar Toast con la notificación de usuario Confirmado

--- frontend/src/views/auth/ConfirmAccountView.vue
+++ frontend/src/views/auth/ConfirmAccountView.vue
@@ -1,25 +1,34 @@
 <script setup>
-import { onMounted } from 'vue';
-import { useRoute } from 'vue-router'
+import { onMounted, inject } from 'vue';
+import { useRoute, useRouter } from 'vue-router'
 import AuthAPI from '../../api/AuthAPI';
 
+const toast = inject('toast')
+const router = useRouter()
 const route = useRoute()
-
 const { token } = route.params;
 
 onMounted(async () => {
   try {
     const { data } = await AuthAPI.verifyAccount(token)
-    console.log(data);
+    toast.open({
+      message: data.msg,
+      type: 'success'
+    })
+    setTimeout(() => {
+      router.push({name: 'login'})
+    }, 5000);
   } catch (error) {
-    console.log(error);
+    toast.open({
+      message: error.response.data.msg,
+      type: 'error'
+    })
   }
 })
 </script>
 
 <template>
-  <div>
-    <h1>Desde ConfirmAccountView</h1>
-
-  </div>
+  <h1 class="text-6xl font-extrabold text-white text-center mt-10">
+    Verificar Cuenta
+  </h1>
 </template>
