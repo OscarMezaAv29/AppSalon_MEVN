db59c0e2cac417066d7f0b4eb03299a0fcdc6d00
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-24 15:59:11 -0600

    Creando el Modelo de Citas / Reservaciones

---
+++ backend/models/Appointment.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const appointmenSchema = mongoose.Schema({
+    services: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Services'
+        }
+    ],
+    date: {
+        type: Date
+    },
+    time: {
+        type: String
+    },
+    totalAmount: {
+        type: Number
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+const Appointment = mongoose.model('Appointment', appointmenSchema)
+
+
+export default Appointment
