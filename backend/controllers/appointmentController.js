3c65809e3f5284e827242a09d774e5072d9d6b7a
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-10-25 15:50:56 -0600

    Definiendo Routing y Controller para Citas

---
+++ backend/controllers/appointmentController.js
@@ -0,0 +1,15 @@
+
+
+
+const createAppointment = async (req, res) => {
+    try {
+        console.log('Desde createAppointment');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export {
+    createAppointment
+}
